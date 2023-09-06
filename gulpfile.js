386a5301cdcaf1ed0a9029416aea0ff1472c232f
S4vi0r <gbp17@outlook.com>
2023-09-06 18:11:18 -0500

    Variables globales en scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done) {
 
@@ -7,17 +8,22 @@ function css(done) {
     /* src('src/scss/app.scss'); */
 
     // Compilarlo
-    /* src('src/scss/app.scss').pipe(); */
+    /* src('src/scss/app.scss').pipe(sass()); */
 
     // Almacerla
-    src('src/scss/app.scss').pipe(sass()).pipe(dest("build/css"));
+    // src('src/scss/app.scss').pipe(sass()).pipe(dest("build/css"));
+    //src('src/scss/**/*.scss').pipe(sass()).pipe(dest("build/css"));
+
+    // Agregamos plumber
+    src('src/scss/**/*.scss').pipe(plumber()).pipe(sass()).pipe(dest("build/css"));
 
     done();
 }
 
 function dev(done) {
 
-    watch('src/scss/app.scss', css);
+    // watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
     done();
 }
 
